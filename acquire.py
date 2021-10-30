19773847a0dccfee7bee54a88ba883080cc7cea2
ian-james-johnson <ian.james.johnson.01@gmail.com>
2021-10-29 23:14:57 -0500

    working on exercises

--- acquire.py
+++ acquire.py
@@ -17,42 +17,64 @@ def parse_gulde_news():
     Returns a dataframe of scraped web-scraping-demo.zgulde.net/news 
     """
     url = 'https://web-scraping-demo.zgulde.net/news' # set the url
-    agent = 'codeup ds germain' # setting the agent so Codeup site doesn't block our web-scraper
-    response = requests.get(url, headers={'User-Agent': agent}) # query
-    soup = BeautifulSoup(response.text) # soup
-    articles = soup.select('.grid.gap-y-12 > div') # raw list of articles
-    article_list = [] # initialise list of dicts for dataframe
-    for article in articles: # parse each article
-        title = article.h2.text # grab title
+    # setting the agent so Codeup site doesn't block our web-scraper
+    agent = 'codeup ds germain' 
+    # query
+    response = requests.get(url, headers={'User-Agent': agent})
+    # soup, features argument is added to prevent a warning
+    # if we don't have features argument, then soup will assume that we want the parser to be for lxml
+    soup = BeautifulSoup(response.text, features="lxml") 
+    # raw list of articles
+    articles = soup.select('.grid.gap-y-12 > div') 
+    # initialise list of dicts for dataframe
+    article_list = [] 
+    # parse each article
+    for article in articles: 
+        # grab title
+        title = article.h2.text 
+        # grab date, author, contents of article
         date, author, contents = article.select('.py-3')[0]\
-            .find_all('p') # grab date, author, contents of article
+            .find_all('p') 
+        # add dict of info to list
         article_list.append({'title':title, 'date':date.text, 
-            'author':author.text, 'contents':contents.text}) # add dict of info to list
-    
+            'author':author.text, 'contents':contents.text}) 
     return pd.DataFrame(article_list) # return dataframe
 
 def parse_gulde_people():
     """ 
     Returns dataframe of scraped web-scraping-demo.zgulde.net/people 
     """
-    url = 'https://web-scraping-demo.zgulde.net/people' # set the url
+    # set the url
+    url = 'https://web-scraping-demo.zgulde.net/people' 
     agent = 'codeup ds germain'
-    response = requests.get(url, headers={'User-Agent': agent}) # query
-    soup = BeautifulSoup(response.text) # soup
-    people = soup.find_all('div', {'class':'person'}) # raw list of people
-    info_list = [] # initialise list of dicts for dataframe
-    for person in people: # parse each person
-        name = person.h2.text # grab name
-        quote, email, phone, address = person.find_all('p') # grab more info
+    # query
+    response = requests.get(url, headers={'User-Agent': agent}) 
+    # soup
+    soup = BeautifulSoup(response.text, features="lxml") 
+    # raw list of people
+    people = soup.find_all('div', {'class':'person'}) 
+    # initialise list of dicts for dataframe
+    info_list = [] 
+    # parse each person
+    for person in people: 
+        # grab name
+        name = person.h2.text 
+        # grab more info
+        quote, email, phone, address = person.find_all('p') 
         # fix info
         quote, email, phone, address = quote.text.strip(), email.text, phone.text, address.text.strip()
-        regexp = r'\s{2,}' # set regex for address fix
-        address = re.sub(regexp, ' ', address) # fix address
+        # set regex for address fix
+        regexp = r'\s{2,}' 
+        # fix address
+        address = re.sub(regexp, ' ', address) 
+        # create dict
         person_dict = {'name':name, 'quote':quote, 'email':email, 
-                       'phone':phone, 'address':address} # create dict
-        info_list.append(person_dict) # add dict to list
+                       'phone':phone, 'address':address} 
+        # add dict to list
+        info_list.append(person_dict) 
 
-    return pd.DataFrame(info_list) # return dataframe
+    # return dataframe
+    return pd.DataFrame(info_list) 
 
 def codeup_blog_urls():
     """ 
@@ -69,19 +91,30 @@ def acquire_codeup_blog(url):
     """ 
     Returns dict of one codeup blog's title, date, category, and content 
     """
-    agent = 'codeup ds germain' # set the agent
-    response = requests.get(url, headers={'User-Agent': agent}) # query
-    soup = BeautifulSoup(response.text, features="lxml") # soup, added features argument to prevent warning
-    title = soup.select('.entry-title')[0].text # get title
-    date = soup.select('.published')[0].text # get date
-    category = soup.find_all('a', {'rel':'category tag'})[0].text # get category
+    # set the agent
+    agent = 'codeup ds germain'
+    # query 
+    response = requests.get(url, headers={'User-Agent': agent}) 
+    # soup, added features argument to prevent warning
+    soup = BeautifulSoup(response.text, features="lxml") 
+    # get title
+    title = soup.select('.entry-title')[0].text 
+    # get date
+    date = soup.select('.published')[0].text 
+    # get category
+    category = soup.find_all('a', {'rel':'category tag'})[0].text 
     # grab all unformatted paragraphs
     paragraphs = soup.find_all('div', {'class':'et_pb_module et_pb_post_content et_pb_post_content_0_tb_body'})[0]\
     .find_all('p')
-    paragraph_list = [] # create list for formatted paragraphs
-    for paragraph in paragraphs: # iterate paragraphs
-        paragraph_list.append(paragraph.text) # add to list
-    content = " ".join(paragraph_list).replace('\xa0', ' ') # destroy href markers
+    # create list for formatted paragraphs
+    paragraph_list = [] 
+    # iterate paragraphs
+    for paragraph in paragraphs: 
+        # add to list
+        paragraph_list.append(paragraph.text) 
+
+    # destroy href markers
+    content = " ".join(paragraph_list).replace('\xa0', ' ') 
     # create dict
     blog_info_dict = {'title':title, 'date':date, 'category':category, 'content':content}
     # return dict
@@ -91,29 +124,44 @@ def get_blogs():
     """ 
     Queries, returns a dataframe of each codeup blog article's stuff 
     """
-    list_of_blog_dicts = [] # initialise the dictionary
+    # initialise the dictionary
+    list_of_blog_dicts = [] 
     for url in codeup_blog_urls():
-        list_of_blog_dicts.append(acquire_codeup_blog(url)) # add each blog to the list of blogs dictionary
+        # add each blog to the list of blogs dictionary
+        list_of_blog_dicts.append(acquire_codeup_blog(url)) 
     return pd.DataFrame(list_of_blog_dicts)
 
 def get_article(url):
     """ 
     Return dataframe of articles in inshorts category URL 
     """
-    agent = 'codeup ds germain' # set the agent
-    response = requests.get(url, headers={'User-Agent': agent}) # query
-    soup = BeautifulSoup(response.text, features="lxml") # soup, added features argument to prevent warning
-    category = soup.find_all('li', {'class':'active-category selected'})[0].text # get category
-    cards = soup.select('.news-card') # get raw cards
-    card_dict_list = [] # create list of dicts for dataframe
-    for card in cards: # iterate each card
-        headline = card.find_all('span', {'itemprop':'headline'})[0].text # headline
-        publish_time = card.find_all('span', {'class':'time'})[0].text # publish time
-        content = card.find_all('div', {'itemprop':'articleBody'})[0].text.strip() # content
+    # set the agent
+    agent = 'codeup ds germain' 
+    # query
+    response = requests.get(url, headers={'User-Agent': agent}) 
+    # soup, added features argument to prevent warning
+    soup = BeautifulSoup(response.text, features="lxml") 
+    # get category
+    category = soup.find_all('li', {'class':'active-category selected'})[0].text 
+    # get raw cards
+    cards = soup.select('.news-card') 
+    # create list of dicts for dataframe
+    card_dict_list = [] 
+    # iterate each card
+    for card in cards: 
+        # headline
+        headline = card.find_all('span', {'itemprop':'headline'})[0].text 
+        # publish time
+        publish_time = card.find_all('span', {'class':'time'})[0].text 
+        # content
+        content = card.find_all('div', {'itemprop':'articleBody'})[0].text.strip() 
+        # create dict
         card_dict = {'headline':headline, 'publish_time':publish_time,
-                       'category':category, 'content':content} # create dict
-        card_dict_list.append(card_dict) # push dict to list
-    return pd.DataFrame(card_dict_list) # return dataframe
+                       'category':category, 'content':content} 
+        # push dict to list
+        card_dict_list.append(card_dict) 
+    # return dataframe
+    return pd.DataFrame(card_dict_list) 
 
 def inshorts_urls():
     """ 
@@ -130,7 +178,11 @@ def get_news():
     Query, return dataframe of inshorts business, 
     sports, tech, entertainment articles 
     """
-    df = pd.DataFrame() # initialise empty dataframe
-    for url in inshorts_urls(): # read each url in list
-        df = pd.concat([df, get_article(url)]) # add each dataframe of cards to df
-    return df # return all urls' cards
+    # initialise empty dataframe
+    df = pd.DataFrame() 
+    # read each url in list
+    for url in inshorts_urls(): 
+        # add each dataframe of cards to df
+        df = pd.concat([df, get_article(url)]) 
+    # return all urls' cards
+    return df 
